160b9e4cab7ffb0e31731bb78cec86d2d6ee38e6
Andrii Liasota <lyasota.andriy@gmail.com>
2022-01-26 15:52:08 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from "@vimeo/player";
+import throttle from 'lodash.throttle';
 
+const STORTIME = "videoplayer-current-time";
+
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const currentTime = localStorage.getItem(STORTIME);
+
+if (currentTime){
+    player.setCurrentTime(currentTime);
+}
+
+
+player.on('timeupdate', throttle((evt) => {
+    localStorage.setItem(STORTIME, evt.seconds)    
+}, 1000) );
