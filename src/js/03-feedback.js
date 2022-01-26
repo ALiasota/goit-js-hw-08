5d68e165ce1ed68c9c45b2bf897b9721a97336c8
Andrii Liasota <lyasota.andriy@gmail.com>
2022-01-26 20:38:11 +0200

    3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,34 @@
+import throttle from 'lodash.throttle';
 
+const STOR_KEY = 'feedback-form-state';
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    email: document.querySelector('input'),
+    message: document.querySelector('textarea')
+}
+
+const formData = {};
+refs.form.addEventListener('input', throttle((e) => {    
+    formData[e.target.name] = e.target.value;    
+    localStorage.setItem(STOR_KEY, JSON.stringify(formData));
+}, 500));
+
+ function insertValues () {
+    const values = localStorage.getItem(STOR_KEY);
+    if (values) {
+        const formData = JSON.parse(values);
+        refs.email.value = formData.email;
+        refs.message.textContent = formData.message;        
+    }
+}
+
+insertValues();
+
+refs.form.addEventListener('submit', (e) => {
+    e.preventDefault();        
+    formData[e.currentTarget.elements[0].name] = e.currentTarget.elements[0].value;
+    formData[e.currentTarget.elements[1].name] = e.currentTarget.elements[1].value;
+    console.log(formData);
+    e.currentTarget.reset();
+    localStorage.removeItem(STOR_KEY);
+})
