ed983f48dcfa9c58d638b91fa0c0e7f90538fe05
Andrii Liasota <lyasota.andriy@gmail.com>
2022-01-30 18:46:26 +0200

    finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -8,7 +8,9 @@ const refs = {
 }
 
 const formData = {};
-refs.form.addEventListener('input', throttle((e) => {    
+refs.form.addEventListener('input', throttle((e) => {
+    formData.email = refs.email.value;
+    formData.message = refs.email.value;    
     formData[e.target.name] = e.target.value;    
     localStorage.setItem(STOR_KEY, JSON.stringify(formData));
 }, 500));
@@ -17,8 +19,15 @@ refs.form.addEventListener('input', throttle((e) => {
     const values = localStorage.getItem(STOR_KEY);
     if (values) {
         const formData = JSON.parse(values);
-        refs.email.value = formData.email;
-        refs.message.textContent = formData.message;        
+        if (formData.email === undefined) {
+            formData.email = "";
+        }
+
+        if (formData.message === undefined) {
+            formData.message = "";
+        }
+        refs.email.value = formData.email;        
+        refs.message.value = formData.message;       
     }
 }
 
@@ -26,10 +35,14 @@ insertValues();
 
 refs.form.addEventListener('submit', (e) => {
     e.preventDefault();   
+    const formSubmit = {};     
+    formSubmit[e.currentTarget.elements[0].name] = e.currentTarget.elements[0].value;
+    formSubmit[e.currentTarget.elements[1].name] = e.currentTarget.elements[1].value;
+    if (e.currentTarget.elements[0].value && e.currentTarget.elements[1].value) {
         
-    formData[e.currentTarget.elements[0].name] = e.currentTarget.elements[0].value;
-    formData[e.currentTarget.elements[1].name] = e.currentTarget.elements[1].value;
-    console.log(formData);
+        
+    console.log(formSubmit);
     e.currentTarget.reset();
     localStorage.removeItem(STOR_KEY);
+    }
 })
