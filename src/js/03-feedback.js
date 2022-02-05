d9a66f184a280b56fe89d63e6137dabec1808f35
Andrii Liasota <lyasota.andriy@gmail.com>
2022-02-05 15:59:06 +0200

    finish

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,16 +1,12 @@
 import throttle from 'lodash.throttle';
 
 const STOR_KEY = 'feedback-form-state';
-const refs = {
-    form: document.querySelector('.feedback-form'),
-    email: document.querySelector('input'),
-    message: document.querySelector('textarea')
-}
 
+const form = document.querySelector('.feedback-form');
 const formData = {};
-refs.form.addEventListener('input', throttle((e) => {
-    formData.email = refs.email.value;
-    formData.message = refs.email.value;    
+form.addEventListener('input', throttle((e) => {
+    formData.email = form.elements.email.value;
+    formData.message = form.elements.message.value;    
     formData[e.target.name] = e.target.value;    
     localStorage.setItem(STOR_KEY, JSON.stringify(formData));
 }, 500));
@@ -26,23 +22,24 @@ refs.form.addEventListener('input', throttle((e) => {
         if (formData.message === undefined) {
             formData.message = "";
         }
-        refs.email.value = formData.email;        
-        refs.message.value = formData.message;       
+        form.elements.email.value = formData.email;        
+        form.elements.message.value = formData.message;       
     }
 }
 
 insertValues();
 
-refs.form.addEventListener('submit', (e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();   
     const formSubmit = {};     
     formSubmit[e.currentTarget.elements[0].name] = e.currentTarget.elements[0].value;
     formSubmit[e.currentTarget.elements[1].name] = e.currentTarget.elements[1].value;
-    if (e.currentTarget.elements[0].value && e.currentTarget.elements[1].value) {
-        
-        
+    if (!e.currentTarget.elements[0].value || !e.currentTarget.elements[1].value) {        
+        alert("Enter email and message");
+        return;
+    }
+
     console.log(formSubmit);
     e.currentTarget.reset();
     localStorage.removeItem(STOR_KEY);
-    }
 })
